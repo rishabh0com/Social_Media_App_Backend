e84a506817aa550390e166a6cf84fa62834f550d
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-10 22:14:34 +0530

    modify postModel

--- models/post.model.js
+++ models/post.model.js
@@ -4,7 +4,8 @@ const postSchema = mongoose.Schema(
     {
         title: { type: String, required: true },
         body: { type: String, required: true },
-        device: { type: String, required: true }
+        device: { type: String, required: true },
+        userId:{type:String, required:true}
     },
     { versionKey: false }
 );
